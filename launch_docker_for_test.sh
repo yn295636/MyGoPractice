e21903ff1728c060c604d1d0d3a23b97447831c5
yining <toyining@gmail.com>
2020-08-19 10:59:05 +0800

    add docker mysql for test

---
+++ launch_docker_for_test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+docker-compose -f docker_compo_test.yml up -d
+MYSQL="mysql -h 127.0.0.1 -P 13306 -uroot -pMytest123! "
+for i in $(seq 60 $END); do
+  echo "SHOW STATUS like 'Connections';" | $MYSQL |& grep "ERROR"
+  result=$?
+  if [[ $result -ne 0 ]]; then
+    echo "SHOW STATUS like 'Connections';" | $MYSQL
+    echo "Check mysql server working status successfully."
+    break
+  fi
+  sleep 1
+done
